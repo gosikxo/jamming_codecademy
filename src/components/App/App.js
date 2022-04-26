bb8762c069a255ca638c06a226b47bc8f5a1b2cd
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-04-26 19:19:01 +0200

    Add SearchBar and SearchResults

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import '././SearchBar.js'
+import SearchBar from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
 
 function App() {
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
-        {/* <!-- Add a SearchBar component --> */}
+        <SearchBar />
         <div className="App-playlist">
-          {/* <!-- Add a SearchResults component -->
-          <!-- Add a Playlist component --> */}
+          <SearchResults />
+          {/* {<!-- Add a Playlist component --> */} 
         </div>
       </div>
     </div>
