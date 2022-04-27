4ea500167f28be1362858682d34058aeab506624
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-04-27 13:57:37 +0200

    Add Tracklist, Playlist and Track components. Fix bug

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
+import { Playlist } from '../Playlist/Playlist';
 
 function App() {
   return (
     <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+    <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
           <SearchResults />
-          {/* {<!-- Add a Playlist component --> */} 
+          <Playlist />
         </div>
       </div>
     </div>
---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './TrackList.css'
+import { Track } from '../Track/Track'
+
+const TrackList = () => {
+    return (
+        <div className="TrackList">
+            {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        </div>
+    )
+}
+
+export default TrackList
